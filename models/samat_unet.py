44b04ce739c9f042c2106174fe1323028e17516e
reason1241 <reason1241@naver.com>
2026-03-02 10:00:48 -0500

    add SmaAtUnet

--- models/samat_unet.py
+++ models/samat_unet.py
@@ -24,7 +24,7 @@ class DepthwiseSeparableConv(nn.Module):
                                    bias=bias)
         
     def forward(self, x):
-        x = self.depthwise()
+        x = self.depthwise(x)
         return self.pointwise(x)
 
 # https://github.com/HansBambel/SmaAt-UNet/blob/snapshot-paper/models/unet_parts.py
@@ -82,9 +82,9 @@ class SpatialAttention(nn.Module):
     def forward(self, x):
         avg_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv(x)
-        attention = torch.sigmoid(x)
+        attention = torch.cat([avg_out, max_out], dim=1)
+        attention = self.conv(attention)
+        attention = self.sigmoid(attention)
         return x * attention
         
 
@@ -99,3 +99,65 @@ class CBAM(nn.Module):
         x = self.channel_attention(x)
         x = self.spatial_attention(x)
         return x
+    
+class SmaAtUNetEncoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = DoubleConv(in_channels, out_channels)
+        self.cbam = CBAM(out_channels)
+        self.pool = nn.MaxPool2d(2)
+    
+    def forward(self, x):
+        x = self.conv(x)
+        skip = self.cbam(x)
+        down = self.pool(x)
+        return down, skip
+
+class SmaAtUNetDecoder(nn.Module):
+    def __init__(self, in_channels, skip_channels, out_channels):
+        super().__init__()
+        self.conv = DoubleConv(in_channels + skip_channels, out_channels)
+        
+    def forward(self, x, skip):
+        # input shape must be divisible by 2^4
+        assert x.shape[-2:] == skip.shape[-2:], (x.shape, skip.shape)
+        
+        # https://docs.pytorch.org/docs/stable/generated/torch.nn.functional.interpolate.html
+        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=False)
+        x = torch.cat([skip, x], dim=1)
+        return self.conv(x)
+        
+class SmaAtUNet(nn.Module):
+    def __init__(self, in_channels=12, out_channels=1, base=64):
+        super().__init__()
+        
+        self.enc1 = SmaAtUNetEncoder(in_channels, base)
+        self.enc2 = SmaAtUNetEncoder(base, base * 2)
+        self.enc3 = SmaAtUNetEncoder(base * 2, base * 4)
+        self.enc4 = SmaAtUNetEncoder(base * 4, base * 8)
+        
+        self.bottleneck_conv = DoubleConv(base * 8, base * 8)
+        self.bottleneck_cbam = CBAM(base * 8)
+        
+        self.dec4 = SmaAtUNetDecoder(base * 8, base * 8, base * 4)
+        self.dec3 = SmaAtUNetDecoder(base * 4, base * 4, base * 2)
+        self.dec2 = SmaAtUNetDecoder(base * 2, base * 2, base)
+        self.dec1 = SmaAtUNetDecoder(base, base, base)
+        
+        self.out = nn.Conv2d(base, out_channels, kernel_size=1)
+        
+    def forward(self, x):
+        x, skip1 = self.enc1(x)
+        x, skip2 = self.enc2(x)
+        x, skip3 = self.enc3(x)
+        x, skip4 = self.enc4(x)
+
+        x = self.bottleneck_conv(x)
+        x = self.bottleneck_cbam(x)
+        
+        x = self.dec4(x, skip4)
+        x = self.dec3(x, skip3)
+        x = self.dec2(x, skip2)
+        x = self.dec1(x, skip1)
+        
+        return self.out(x)
